8ff1cc1527aea3b356e6e4ac74e8ad97acb9f1ab
MMcClure11 <mcclme02@luther.edu>
2020-08-20 09:41:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function addingEventListener() {
-  const input = document.getElementById('input');
+const input = document.getElementById('input');
+
+function addingEventListener() {  
   input.addEventListener('click', function(event) {
     alert('I was clicked!');
   });
 }
+
+
+//learnco solution
+// const input = document.querySelector('input');
+
+// function addingEventListener() {
+//   input.addEventListener('click', function(event) {
+//     return "clicked"
+//   });
+// }
