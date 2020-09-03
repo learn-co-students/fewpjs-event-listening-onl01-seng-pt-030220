076007ba4834c781bbd1e52c18299eb45b907e3a
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-09-02 22:11:43 -0700

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,13 @@
       alert('I was clicked!');
     });
   }
+
+
+
+// const input = document.querySelector('input');
+
+// function addingEventListener() {
+//   input.addEventListener('click', function(event) {
+//     return "clicked"
+//   });
+// }
